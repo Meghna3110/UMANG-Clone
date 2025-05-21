589d58681b7d4e7d66527d8aea74d91c4a38e75b
Meghna3110 <meghnatontanal@gmail.com>
2025-05-21 12:21:12 +0530

    added department details component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { ServiceByStateComponent } from './service-by-state/service-by-state.component';
+import { DepartmentDetailsComponent } from './department-details/department-details.component';
 
 export const routes: Routes = [
     {path: '',component: HomeComponent},
     {path: '**', redirectTo: ''},
-    {path: 'services', component: ServiceByStateComponent}
+    {path: 'services', component: ServiceByStateComponent},
+    {path: 'department-details', component: DepartmentDetailsComponent}
 ];
 
 @NgModule({
